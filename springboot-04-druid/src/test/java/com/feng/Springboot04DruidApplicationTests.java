889ef3bf050854b847ac9a1dbff3b560500b0b75
fengyu415 <969170653@qq.com>
2020-08-13 17:50:32 +0800

    Druid

---
+++ springboot-04-druid/src/test/java/com/feng/Springboot04DruidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04DruidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
